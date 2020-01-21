d3c6162e3e7c0a1b3a313fd0363f9aa5eafcf8c7
Feather Knee <featherknee@gmail.com>
2020-01-20 17:16:46 -0800

    add empty snapshots

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders', () => {
   const { asFragment } = render(<App />);
   expect(asFragment).toMatchSnapshot();
 });
