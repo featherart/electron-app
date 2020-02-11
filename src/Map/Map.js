cfeaa67c28a6f0b41f0318d469c2f63aea15c379
Feather Knee <featherknee@gmail.com>
2020-02-10 22:58:01 -0800

    map rendering without React wrapper

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,33 +1,50 @@
 import React from 'react';
-import ReactMapGL from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Markers from './Markers';
+import './map.css';
+//import Markers from './Markers';
 
-const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-const geolocateStyle = {
-  padding: '10px'
-};
+export class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -122.43,
+      lat: 37.837,
+      zoom: 12
+    };
+  }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/satellite-v9',
+      center: [ this.state.lng, this.state.lat ],
+      zoom: this.state.zoom
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
+}
 
-export const Map = props => {
-  const { markers } = props;
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-      <span style={{ display: 'flex', fontSize: '15px', fontWeight: 'bolder' }}>
-        Map
-      </span>
-      <ReactMapGL
-        style={geolocateStyle}
-        mapStyle={'mapbox://styles/mapbox/satellite-v9'}
-        mapboxApiAccessToken={TOKEN}
-        width={'100vw'}
-        height={'100vh'}
-        latitude={37.8370}
-        longitude={-122.4300}
-        zoom={12}
-      >
-        <Markers data={markers} />
-      </ReactMapGL>
-    </div>
-  );
-};
+/*
+<ReactMapGL
+  {...viewport}
+  onClick={() => console.log('hihihihi')}
+  doubleClickZoom={false}
+  style={geolocateStyle}
+  mapStyle={'mapbox://styles/mapbox/satellite-v9'}
+  mapboxApiAccessToken={TOKEN}
+  width={'100vw'}
+  height={'100vh'}
+  latitude={37.837}
+  longitude={-122.43}
+  zoom={12}
+>
+</ReactMapGL>
+*/
--- src/Map/Markers.js
+++ src/Map/Markers.js
@@ -1,17 +1,45 @@
 import React, { PureComponent } from 'react';
-import { Marker } from 'react-map-gl';
+import { Marker, Popup } from 'react-map-gl';
 
 // PureComponent ensures that the markers are only rerendered when data changes
 class Markers extends PureComponent {
+  state = {
+    showPopup: true
+  };
+
   render() {
     const { data } = this.props;
     return data.map(marker => {
-      const { lon, lat, imageName } = marker
-      const { fields: { file: { url }} } = imageName
+      const { lon, lat, imageName } = marker;
+      const { fields: { file: { url } } } = imageName;
+
       return (
-        <Marker key={lon} longitude={lon} latitude={lat}>
-          <img alt="marker" height="48px" src={url} />
-        </Marker>
+        <div key={lon}>
+          {this.state.showPopup && (
+            <Popup
+              latitude={lat}
+              captureClick={true}
+              longitude={lon}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => this.setState({ showPopup: false })}
+              anchor="right"
+            >
+              <button onClick={() => console.log('hibye')} />
+            </Popup>
+          )}
+          <Marker longitude={lon} latitude={lat}>
+            <button
+              style={{ backgroundColor: 'yellow', height: '52px', width: '52px', borderRadius: '50%' }}
+              onClick={e => {
+                console.log('hiihihihi')
+                this.setState({ showPopup: true })
+              }}
+            >
+              <img alt="marker" height="52px" src={url} />
+            </button>
+          </Marker>
+        </div>
       );
     });
   }
