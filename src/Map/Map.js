021f52363976b3ea1d671573b111804daf7e0f50
Feather Knee <featherknee@gmail.com>
2020-02-15 22:59:37 -0800

    add popup

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
+import React, { useState } from 'react';
+import ReactMapboxGl, { Marker, Popup } from 'react-mapbox-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './map.css';
@@ -8,10 +8,11 @@ export const MapView = ({ markers }) => {
   const Map = ReactMapboxGl({
     accessToken: process.env.REACT_APP_MAPBOX_TOKEN
   });
+  const [ showPopup, togglePopup ] = useState(false);
 
   return (
     <Map
-      style={"mapbox://styles/mapbox/satellite-v9"}
+      style={'mapbox://styles/mapbox/satellite-v9'}
       containerStyle={{
         height: '100vh',
         width: '100vw'
@@ -23,15 +24,27 @@ export const MapView = ({ markers }) => {
       {markers.map(marker => {
         const { lon, lat, url, imageName } = marker;
         return (
-        <Marker
-          key={lat}
-          coordinates={[ lon, lat ]}
-          anchor="bottom"
-          onClick={() => console.log('clicked on', lon, lat)}
-        >
-          <img alt={imageName} height="50" width="50" src={marker.url} />
-        </Marker>
-      )})}
+          <Marker
+            key={lat}
+            coordinates={[ lon, lat ]}
+            anchor="bottom"
+            onClick={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              //togglePopup(!showPopup)
+            }}
+            >
+              {
+                showPopup &&
+                <Popup
+                  coordinates={[ lon, lat ]}
+                  offset={{'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]}}
+                />
+              }
+            <img alt={imageName} height="40" width="40" src={url} />
+          </Marker>
+        );
+      })}
     </Map>
   );
 };
